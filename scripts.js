cec95053cc61e676279c6412318abb47220e89b5
Anaïs Pease <115511681+AlpOnCaffeine@users.noreply.github.com>
2025-03-03 02:25:45 -0600

    masssssssss
    donew

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+//Script for the carousel
 document.addEventListener('DOMContentLoaded', function() {
     const gridViewBtn = document.getElementById('gridViewBtn');
     const carouselViewBtn = document.getElementById('carouselViewBtn');
@@ -11,5 +12,31 @@ document.addEventListener('DOMContentLoaded', function() {
     carouselViewBtn.addEventListener('click', function() {
         productContainer.classList.add('carousel-view');
         productContainer.classList.remove('grid-view');
+    });
+});
+
+//Script for the email and phone messages
+function showContactField(method) {
+    var phoneInput = document.getElementById('phoneInput');
+    var emailInput = document.getElementById('emailInput');
+    if (method === 'phone') {
+        phoneInput.style.display = 'block';
+        emailInput.style.display = 'none';
+    } else if (method === 'email') {
+        emailInput.style.display = 'block';
+        phoneInput.style.display = 'none';
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.product-container .product-item img').forEach(item => {
+        item.addEventListener('click', function() {
+            document.getElementById('lightboxModal').style.display = "block";
+            document.getElementById('lightboxImg').src = this.src;
+            document.getElementById('caption').innerHTML = this.alt;
+        });
+    });
+
+    document.querySelector('.close').addEventListener('click', function() {
+        document.getElementById('lightboxModal').style.display = "none";
     });
 });
